dce2b7aad5f741b598056c50266090110ad7a521
tapurou <r202202223wk@jindai.jp>
2022-11-14 20:33:49 +0900

    20221114

---
+++ de12/picturefile/de12/python/kadai2.py
@@ -0,0 +1,27 @@
+from symbol import namedexpr_test
+
+
+name = int(input('閲覧する株価の種類の数字を選んでください。1,金融機関 2,マスメディア 3,通信サービス　4,システム・ソフトウェア 5,小売り'))
+a = ["stock.py", "systemsoftware.py", "retail.py", "finamce.py", "massmedia.py"]
+if name == 1:
+    
+print(a[0])
+print(a[1])
+print(a[2])
+print(a[3])
+print(a[4])
+
+import webbrowser
+webbrowser.open('https://www.nikkei.com/nkd/industry/stocklist/?n_m_code=121')  #金融機関
+
+import webbrowser
+webbrowser.open('https://www.nikkei.com/nkd/industry/stocklist/?n_m_code=141')  #マスメディア
+
+import webbrowser
+webbrowser.open('https://www.nikkei.com/nkd/industry/stocklist/?n_m_code=101')  #小売り
+
+import webbrowser
+webbrowser.open('https://www.nikkei.com/nkd/industry/stocklist/?n_m_code=146')  #システム・ソフトウェア
+
+import webbrowser
+webbrowser.open('https://www.nikkei.com/nkd/industry/stocklist/?n_m_code=142')  #通信サービス
---
+++ de12/picturefile/de12/python/work2.py
@@ -0,0 +1,15 @@
+#ageという変数に44を代入している
+name="aaa"
+#nameという変数に84を代入している
+waist=84
+#waistという変数にaaaを代入している
+age=44
+
+
+print(name, "さんは腹囲", waist, "cmで年齢は",age, "才ですね。")
+
+
+if waist>=85 and age>=40:
+    print(name,"さん、内臓脂肪蓄積注意です")
+else:
+    print(name,"さん、腹囲は問題ありません")
